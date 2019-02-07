28f3e6ad334bc694c74e74a8405aac6af8b0e6cd
adrian19M <42873010+adrian19M@users.noreply.github.com>
2019-02-07 09:25:41 -0600

    Create reto_3.js

---
+++ reto_3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+	strokeWeight(10);
+	stroke(255,0,0);
+	ellipse(200,200,250,250);
+	strokeWeight(10);
+	line(117,117,287,287);
+  
+}
