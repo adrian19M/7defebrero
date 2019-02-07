93e77105ec1a756e51fae1ea0575b85223051770
adrian19M <42873010+adrian19M@users.noreply.github.com>
2019-02-07 11:58:18 -0600

    Create reto_4.js

---
+++ reto_4.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+	if(mouseIsPressed){
+  background(0,0,255);
+	rect(150,150,80,80);
+		textSize(100);
+		text("hola",80,130,130);
+		
+}
+	else {
+	background(150);
+	rect(150,150,80,80);
+	
+	
+	}
+	
+} 
